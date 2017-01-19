ed2417ddd5861701f40745374d7990023f3fe46b
zhuwenjie11 <48839949@qq.com>
2017-01-19 16:56:46 +0800

    【修改】提交描述

--- devops-dashboard/src/main/java/com/eastcom/demo/devops/DevOpsWebStart.java
+++ devops-dashboard/src/main/java/com/eastcom/demo/devops/DevOpsWebStart.java
@@ -2,8 +2,12 @@ package com.eastcom.demo.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class DevOpsWebStart {
 
 	public static void main(String[] args) {
