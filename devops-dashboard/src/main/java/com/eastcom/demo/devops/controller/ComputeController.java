207f8112774718c2ec78e71e02c09269ea4641f8
zhuwenjie11 <48839949@qq.com>
2017-01-19 16:50:59 +0800

    【修改】增加描述

--- devops-dashboard/src/main/java/com/eastcom/demo/devops/controller/ComputeController.java
+++ devops-dashboard/src/main/java/com/eastcom/demo/devops/controller/ComputeController.java
@@ -2,24 +2,33 @@ package com.eastcom.demo.devops.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class ComputeController {
 
-	private Logger logger = LoggerFactory.getLogger(TimeController.class);
+	private Logger logger = LoggerFactory.getLogger(ComputeController.class);
 	
 	@GetMapping("/addnum")
+	@HystrixCommand(fallbackMethod = "ServiceUnavailable")
     String add(@RequestParam Integer a,@RequestParam Integer b) {
 		logger.info("call addnum start.");
 		RestTemplate rest = new RestTemplate();
+	
 		String reuslt = rest.getForEntity("http://devops-compute:8101/add?a="+a+"&b="+b, String.class).getBody();
 		logger.info("call addnum end , return result {}",reuslt);
-		reuslt = "result : a + b = "+reuslt+" !";
+		reuslt = "result : a + b = "+reuslt+" ! test";
         return reuslt;
     }
+	
+	public String ServiceUnavailable(Integer a , Integer b){
+		return "Service Unavailable! a + b ";
+	}
 	
 }
